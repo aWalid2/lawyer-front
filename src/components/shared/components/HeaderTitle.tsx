c91ece153236fd03e79de3a31e65abef74e8c695
Hasan Elmaghraby <maghrabyhasan@gmail.com>
2026-03-06 00:02:03 +0200

    create logo loading

--- src/components/shared/components/HeaderTitle.tsx
+++ src/components/shared/components/HeaderTitle.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 import { ArrowRightIcon } from "../icons/ArrowRight";
+import { useNavigate } from "react-router-dom";
 
 interface HeaderTitleProps {
   title: string;
 }
 
 export const HeaderTitle: React.FC<HeaderTitleProps> = ({ title }) => {
+  const navigate = useNavigate();
   return (
-    <div className="flex items-center gap-2 text-secondary">
+    <div onClick={() => navigate(-1)} className="flex items-center gap-2 text-secondary">
       <ArrowRightIcon className="text-secondary" />
       <h2 className="text-[20px] font-semibold">{title}</h2>
     </div>
