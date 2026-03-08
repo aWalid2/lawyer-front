9da8bed2d6d0d1e78adc4ce2dc022b6d5d7e6200
Hasan Elmaghraby <maghrabyhasan@gmail.com>
2026-03-08 17:45:48 +0200

    fix some imports

--- src/features/dashboard/components/SidebarDashboard/index.tsx
+++ src/features/dashboard/components/SidebarDashboard/index.tsx
@@ -1,5 +1,5 @@
 import { Menu } from "lucide-react";
-import logo from "../../../../../public/images/logo.webp";
+import logo from "@/public/images/logo.webp";
 import {
   Sheet,
   SheetContent,
--- vite.config.ts
+++ vite.config.ts
@@ -8,8 +8,8 @@ export default defineConfig({
     plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
       "@/public": path.resolve(__dirname, "./public"),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
 })
