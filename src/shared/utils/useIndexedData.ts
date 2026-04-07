b2316b0604e475eb1ce2da794f740535808f6ec5
Hasan Elmaghraby <maghrabyhasan@gmail.com>
2026-04-07 21:54:59 +0200

    udpate indexed data

--- src/shared/utils/useIndexedData.ts
+++ src/shared/utils/useIndexedData.ts
@@ -1,12 +1,13 @@
 import { useMemo } from "react";
 
-export const useIndexedData = (data: any[]) => {
+export const useIndexedData = (data: any[], page: number = 1, limit: number = 10) => {
     const indexedData = useMemo(() => {
         return (data ?? []).map((item: any, index: number) => ({
             ...item,
-            rowNumber: index + 1,
+            rowNumber: ((page - 1) * limit) + index + 1
+
         }));
-    }, [data]);
+    }, [data, page, limit]);
     return indexedData;
 }
 
