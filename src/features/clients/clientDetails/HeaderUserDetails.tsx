dbb9ea73ebfa9e5f874726bd1db5c65069729945
Hasan Elmaghraby <maghrabyhasan@gmail.com>
2026-04-08 22:53:53 +0200

    finish task without add contract

--- src/features/clients/clientDetails/HeaderUserDetails.tsx
+++ src/features/clients/clientDetails/HeaderUserDetails.tsx
@@ -1,11 +1,13 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import { Plus, X } from "lucide-react";
+import { Link, useNavigate } from "react-router-dom";
+import { Plus } from "lucide-react";
 import { HeaderTitle } from "@/shared/components/HeaderTitle";
 import { EditIcon } from "@/shared/icons/Edit";
 import { TrashIcon } from "@/shared/icons/Trash";
 import { AddContractDialog } from "./AddContractDialog";
 import { EditClientDialog } from "../clients/components/EditClientDialog";
+import { ConfirmDeleteDialog } from "@/shared/components/ConfirmDeleteDialog";
+import { useDeleteClient } from "../api/hooks/useDeleteClient";
 
 interface HeaderUserDetailsProps {
   activeTab?: string;
@@ -16,6 +18,8 @@ export const HeaderUserDetails: React.FC<HeaderUserDetailsProps> = ({
   activeTab,
   client,
 }) => {
+  const { mutate: deleteClient } = useDeleteClient();
+  const navigate = useNavigate();
   return (
     <div className="flex items-center  flex-wrap gap-y-3 justify-between mb-6">
       <HeaderTitle title="تفاصيل الموكل" />
@@ -32,16 +36,24 @@ export const HeaderUserDetails: React.FC<HeaderUserDetailsProps> = ({
             }
           />
 
-          <button className="bg-[#C60000]/8 hover:bg-red-100 text-[#C60000] p-3 rounded-[8px] shadow-sm transition-all h-12.5 w-12.5 flex items-center justify-center">
-            <TrashIcon />
-          </button>
+          <ConfirmDeleteDialog
+            trigger={
+              <button className="bg-[#C60000]/8 hover:bg-red-100 text-[#C60000] p-3 rounded-[8px] shadow-sm transition-all h-12.5 w-12.5 flex items-center justify-center">
+                <TrashIcon />
+              </button>
+            }
+            onConfirm={async () => {
+              await deleteClient({ id: client.user_id })
+              navigate("/dashboard/clients")
+            }}
+          />
 
           <AddContractDialog />
         </div>
       ) : (
         <Link
           to="/dashboard/case-management/add-case"
-          className="bg-primary-gradient text-white px-6 py-2.5 rounded-[12px] font-semibold flex items-center gap-2 shadow-lg h-12.5 hover:shadow-xl transition-all"
+          className="bg-primary-gradient text-white px-6 py-2.5 rounded-mainfont-semibold flex items-center gap-2 shadow-lg h-12.5 hover:shadow-xl transition-all"
         >
           <Plus size={20} />
           قضية جديدة
