0dda0039deafa8e1f6fe9375d28848036caae374
maged135 <maged.mego2018@gmail.com>
2026-04-11 13:26:55 +0200

    shhs

--- src/features/clients/addclient/types/addClientT.ts
+++ src/features/clients/addclient/types/addClientT.ts
@@ -1,31 +1,9 @@
-// export interface FormValues {
-//   first_name: string;
-//   email: string;
-//   password: string;
-//   contract_file: File | null;
-//   authorization_photo: File | null;
-//   nationality: string;
-//   country: string;
-//   address: string;
-//   ssn: string;
-//   phone: string;
-//   countryCode: string;
-//   has_contract: boolean;
-//   add_clients: boolean;
-//   client_type: "individual" | "company" | "government";
-//   notes: string;
-//   contract_start_date: string;
-//   contract_value: string;
-//   contract_duration: string;
-//   confirmation_password: string;
-// }
 
-// features/clients/addclient/types/addClientT.ts
 export interface FormValues {
   first_name: string;
   email: string;
   password: string;
-  contract_file: File | null;
+  contract_photo: File | null;
   authorization_photo: File | null;
   nationality: string;
   country: string;
