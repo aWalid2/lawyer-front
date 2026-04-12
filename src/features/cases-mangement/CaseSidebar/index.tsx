7da742cf40a591c78403b77ed09d528982a7eb0f
Hasan Elmaghraby <maghrabyhasan@gmail.com>
2026-04-12 18:07:31 +0200

    create case info

--- src/features/cases-mangement/CaseSidebar/index.tsx
+++ src/features/cases-mangement/CaseSidebar/index.tsx
@@ -1,18 +1,21 @@
 import React from "react";
 import { CaseSummaryCard } from "./Components/CaseSummaryCard";
 import { NavigationMenu } from "./Components/NavigationMenu";
+import { useGetCaseInfo } from "../CaseInfo/api/hooks/useGetCaseInfo";
+import { useParams } from "react-router-dom";
+import LoadingPage from "@/shared/components/LoadingPage";
+import { Error } from "@/shared/components/Error";
 
-const caseData = {
-  caseTitle: "قضية سرقة",
-  autoNumber: "543257",
-  clientName: "علي العتيبي",
-  clientCode: "543257",
-};
+
 
 export const CaseSidebar: React.FC = () => {
+  const { id } = useParams();
+  const { data: caseInfo, isPending, isError, isSuccess } = useGetCaseInfo(id!) || {};
   return (
-    <aside className="w-full xl:w-[273px] bg-white rounded-[12px] shadow-primary py-6 px-4 flex flex-col gap-6 h-fit">
-      <CaseSummaryCard caseData={caseData} />
+    <aside className="w-full xl:w-[273px] bg-white rounded-main shadow-primary py-6 px-4 flex flex-col gap-6 h-fit">
+      {isPending && <LoadingPage />}
+      {isError && <Error />}
+      {isSuccess && <CaseSummaryCard caseData={caseInfo} />}
 
       <NavigationMenu />
     </aside>
