b0569a41abcf0253fb77f4cdb0571af129b63bee
Hasan Elmaghraby <maghrabyhasan@gmail.com>
2026-04-20 18:56:21 +0200

    prepare reports

--- src/features/cases-mangement/sessions/ExpertSessions/components/ExpertsSessionInfo/components/HeaderExpertsSessionInfo.tsx
+++ src/features/cases-mangement/sessions/ExpertSessions/components/ExpertsSessionInfo/components/HeaderExpertsSessionInfo.tsx
@@ -1,25 +1,11 @@
-import Frame from "@/public/images/Frame.svg";
-import React from "react";
 
-interface HeaderExpertsSessionInfoProps {
-    handleEditClick: () => void;
-}
 
-export const HeaderExpertsSessionInfo: React.FC<HeaderExpertsSessionInfoProps> = ({ handleEditClick }) => {
+
+
+export const HeaderExpertsSessionInfo = () => {
     return (
         <div className="flex justify-between items-center pb-8">
-            <h1 className="text-xl font-cairo">بيانات الخبراء</h1>
-
-            <button
-                type="button"
-                onClick={handleEditClick}
-                className="flex shrink-0 items-center justify-center gap-2 bg-[#F1F1F3] rounded-md w-[114px] h-[50px] hover:bg-[#E1E1E6] transition-colors duration-200 px-2 sm:w-auto sm:px-4"
-            >
-                <img src={Frame} alt="" className="w-5 h-5 sm:w-4 sm:h-4" />
-                <span className="text-[16px] font-medium whitespace-nowrap">
-                    تعديل
-                </span>
-            </button>
+            <h1 className="text-xl font-cairo">بيانات اخر تقرير</h1>
         </div>
     );
-};
+}
--- src/features/cases-mangement/sessions/ExpertSessions/components/ExpertsSessionInfo/index.tsx
+++ src/features/cases-mangement/sessions/ExpertSessions/components/ExpertsSessionInfo/index.tsx
@@ -1,114 +1,43 @@
 
-import { useState } from "react";
-import { EditModelExperts } from "./components/EditModelExperts";
 import { ExpertsSessionBox } from "./components/ExpertsSessionBox";
 import { HeaderExpertsSessionInfo } from "./components/HeaderExpertsSessionInfo";
 
 
 
 export const ExpertsSessionInfo = () => {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [formValues, setFormValues] = useState({
-    expertReportNumber: "",
-    assignedAuthority: "",
-    assignmentDate: "",
-    expertOfficeName: "",
-    taskStartDate: "",
-    subjectOfExpertise: "",
-    finalOpinion: "",
-    reportSubmissionDate: "",
-    objections: "",
-  });
-
-  const handleEditClick = () => {
-    setIsModalOpen(true);
-  };
-
-  const handleSaveChanges = (values: any) => {
-    console.log("تم الحفظ:", values);
-    setFormValues({
-      expertReportNumber: values.expertReportNumber,
-      assignedAuthority: values.assignedAuthority,
-      assignmentDate: values.assignmentDate,
-      expertOfficeName: values.expertOfficeName,
-      taskStartDate: values.taskStartDate,
-      subjectOfExpertise: values.subjectOfExpertise,
-      finalOpinion: values.finalOpinion,
-      reportSubmissionDate: values.reportSubmissionDate,
-      objections: values.objections,
-    });
-  };
-
-  const documentForModal = {
-    id: "1",
-    expertReportNumber: formValues.expertReportNumber,
-    assignedAuthority: formValues.assignedAuthority,
-    assignmentDate: formValues.assignmentDate,
-    expertOfficeName: formValues.expertOfficeName,
-    taskStartDate: formValues.taskStartDate,
-    subjectOfExpertise: formValues.subjectOfExpertise,
-    finalOpinion: formValues.finalOpinion,
-    reportSubmissionDate: formValues.reportSubmissionDate,
-    objections: formValues.objections,
-    type: "experts",
-  };
 
   return (
     <>
-      <div className="border border-gray-300 p-4 rounded-xl mb-6">
-        <HeaderExpertsSessionInfo handleEditClick={handleEditClick} />
-        <div className="space-y-6 grid md:grid-cols-2 gap-4">
+      <div className="border border-gray-300 p-6 rounded-xl mb-6">
+        <HeaderExpertsSessionInfo />
+        <div className="grid md:grid-cols-2 gap-6">
           <ExpertsSessionBox
-            label="رقم تقرير الخبير"
-            text={"123"}
-          />
-          <ExpertsSessionBox
-            label="الجهة المكلفة"
-            text={"123"}
+            label="مكتب الخبراء / الخبير"
+            text={"المكتب العربي للاستشارات الهندسية"}
           />
 
           <ExpertsSessionBox
-            label="تاريخ التكليف"
-            text={"123"}
-          />
-          <ExpertsSessionBox
-            label="مكتب الخبراء / الخبير"
-            text={"123"}
-          />
-          <ExpertsSessionBox
-            label="تاريخ مباشرة المهمة"
-            text={"123"}
-          />
-          <ExpertsSessionBox
             label="موضوع الخبرة"
-            text={"123"}
+            text={"مراجعة الاعمال الانشائية"}
           />
 
           <ExpertsSessionBox
             label="الرأي النهائي للخبير"
-            text={"123"}
+            text={"التقرير النهائي "}
           />
           <ExpertsSessionBox
             label="تاريخ إيداع التقرير"
-            text={"123"}
+            text={"2026-04-20"}
           />
 
           <ExpertsSessionBox
-            label="الاعتراضات"
-            text={"123"}
+            label="الحالة"
+            text={"تم الإيداع"}
           />
 
-          <ExpertsSessionBox
-            label="ملاحظات إضافية"
-            text={"123"}
-          />
+
         </div>
-        <EditModelExperts
-          document={documentForModal}
-          open={isModalOpen}
-          onOpenChange={setIsModalOpen}
-          onSave={handleSaveChanges}
-        />
+
       </div>
     </>
   );
